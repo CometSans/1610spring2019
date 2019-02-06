fc49e941bbb58251a1749e73afb265ae56b41dd6
Angel Torgerson <angeltorgerson@mac.com>
2019-02-05 23:06:46 -0700

    Created Classes

---
+++ First Game (Yosh)/Assets/FlyingAnimals.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlyingAnimals : Animal
+{
+
+	public Color WingColor;
+	
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ First Game (Yosh)/Assets/LandAnimal.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LandAnimal : Animal
+{
+
+	public Color FeetColor;
+	public Color TailColor;
+	
+	
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
