7d7831adb415c2ff5ffa759de3051412f37a79f2
Angel Torgerson <angeltorgerson@mac.com>
2019-03-31 22:06:32 -0600

    If/Else and FloatData

---
+++ First Game (Yosh)/Assets/Scripts/Conditional.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Conditional : MonoBehaviour
+{
+
+	public bool CanWalk;
+	public int Number = 11;
+	public string Password;
+	
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		if (CanWalk)
+		{
+			print("True");
+		}
+		else
+		{
+			print("False");
+		}
+
+		if (Number >= 10)
+		{
+			print("Greater");
+		}
+		else
+		{
+			print("Lesser");
+		}
+
+		if (Password == "OU812")
+		{
+			print("Correct");
+		}
+		else
+		{
+			print("Incorrect");
+		}
+		
+		
+	}
+}
