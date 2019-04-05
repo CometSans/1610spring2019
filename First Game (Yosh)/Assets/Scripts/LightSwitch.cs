a0cd5816dc58f07e12eb1e3125abc69539cc618a
Angel Torgerson <angeltorgerson@mac.com>
2019-04-04 22:43:03 -0600

    LightSwitch?

--- First Game (Yosh)/Assets/Scripts/LightSwitch.cs
+++ First Game (Yosh)/Assets/Scripts/LightSwitch.cs
@@ -1,17 +1,19 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class LightSwitch : MonoBehaviour
 {
 
 	public string CodeWord;
+	public UnityEvent LightEvent;
 
 	void Update()
 	{
 		if (CodeWord == "Wassup")
 		{
-			light.GetComponent<>(); 
+			LightEvent.Invoke();
 		}
 	}
 	
