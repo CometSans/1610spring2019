3b696456c22015b033d41c49c75a4fe885a24960
Angel Torgerson <angeltorgerson@mac.com>
2019-04-11 22:38:02 -0600

    Hip

--- First Game (Yosh)/Assets/Scripts/WaterAnimal.cs
+++ First Game (Yosh)/Assets/Scripts/WaterAnimal.cs
@@ -6,13 +6,13 @@ public class WaterAnimal : Animal
 {
 
 	public Color FinColor;
-	
+	public float DamageLevel;
 	public bool CanWalkOnLand;
 	
 	// Use this for initialization
 	void Start ()
 	{
-		print("Hello World!");
+		
 	}
 	
 	// Update is called once per frame
